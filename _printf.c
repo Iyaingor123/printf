f6d635efdbab9752864a293db735d473d0e833b0
Iyaingor123 <pri_dekk2001@yahoo.com>
2023-10-17 14:57:16 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -4,25 +4,4 @@
 /**
  * _printf - function to print text
  * @format: format specifier
- * Return: number of characters printed
- **/
-
-int _printf(const char *format, ...)
-{
-	int count = 0;
-
-	va_list args;
-}
-if (*format == NULL)
-	return (-1);
-	va_start(args, format);
-	while (format);
-{
-	if (*format != '%')
-		write(1, format, 1)
-}
-else
-{
-	format++;
-		return (count);
-}
+ * Return: number of 
--- _putchar.c
+++ _putchar.c
@@ -4,10 +4,5 @@
 /**
  * _putchar - function that writes the character
  * @c: print char
- * Retun: 1 otherwise failure
+ * Return: 1 otherwise failure
  **/
-
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
